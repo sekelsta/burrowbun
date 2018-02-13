fe5dcd3febad33da7391aa31a39a196a1339bfbb
Tiffany Jager <tiffanyann.jager@gmail.com>
2018-02-12 20:54:36 -0800

    now correctly finds the path even when it's not in argv[0]

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,21 @@
 #include "Game.hh"
 #include <iostream>
 #include <string>
+#include <libgen.h> // For dirname
+#include <unistd.h> // For readlink
+
+using namespace std;
 
 int main(int argc, char **argv) {
-    /* The name of the executable, without any path. */
-    std::string executable = "burrowbun";
-    std::string called = std::string(argv[0]);
-
-    /* The path to the executable. */
-    std::string path = called.substr(0, called.size() - executable.size());
+    /* The path to the executable, linux-only. */
+    int PATH_MAX = 512;
+    char result[ PATH_MAX ];
+    ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+    std::string path;
+    if (count != -1) {
+        path = dirname(result);
+    }
+    path = path + "/";
 
     Game game(path);
     game.run();
