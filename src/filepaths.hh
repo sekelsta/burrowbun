cbf90361d67affb8e236a60b87a75cf20ec70e20
Tiffany Jager <tjager@caltech.edu>
2018-04-14 13:43:47 -0700

    added images for later use

--- src/filepaths.hh
+++ src/filepaths.hh
@@ -3,7 +3,7 @@
 
 /* The paths to image files. */
 #define TILE_SPRITE_PATH "content/Blocks/"
-#define MOVABLE_SPRITE_PATH "content/"
+#define MOVABLE_SPRITE_PATH "content/Entities/"
 #define UI_SPRITE_PATH "content/"
 #define ICON_SPRITE_PATH "content/Icons/"
 #define FONT_FILE_PATH "fonts/"
